7696ea58b7c4cd7ba74b07e5f091e85e6aee758b
Chuka Okoye <contact@chookah.org>
2014-08-25 15:10:24 -0700

    updated decorators to support keywords

--- tryfer/decorators.py
+++ tryfer/decorators.py
@@ -7,44 +7,48 @@ from socket import gethostname, gethostbyname
 from os import environ
 import logging
 import random
+import functools
 
 tracers = [DebugTracer(), ZipkinTracer()]
 function_name = lambda: random.choice(['GET', 'POST', 'PUT', 'DELETE', 'HEAD'])
 
 
-def rpc_zipper(func):
-    parent_trace = None
-    def wrapper(*args, service_name='waldo', **kwargs):
-        '''
-        do what ever we need to setup zipkin and set appropriate headers
-        '''
-        #first, create a trace using appropriate headers
-        if not parent_trace:
-            trace = Trace(function_name(),
+def rpc_zipper(service_name='waldo'):
+    def zipper(func):
+        parent_trace = None
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            '''
+            do what ever we need to setup zipkin and set appropriate headers
+            '''
+            #first, create a trace using appropriate headers
+            if not parent_trace:
+                trace = Trace(function_name(),
                             None,
                             None,
                             None,
                             tracers=tracers)
-        else:
-            trace = parent_trace.child(function_name())
-        #ideally, these info  would be extracted from request args.
-        #hacky way to determine hostname, port and service_name
-        host_name = gethostbyname(gethostname())
-        host_port = 80
-        service_name = service_name
-        endpoint = Endpoint(host_name, host_port, service_name)
-        trace.set_endpoint(endpoint)
+            else:
+                trace = parent_trace.child(function_name())
+            #ideally, these info  would be extracted from request args.
+            #hacky way to determine hostname, port and service_name
+            host_name = gethostbyname(gethostname())
+            host_port = 80
+            service_name = service_name
+            endpoint = Endpoint(host_name, host_port, service_name)
+            trace.set_endpoint(endpoint)
 
-        #now, log start point
-        logging.debug('recording server recv span')
-        trace.record(Annotation.server_recv())
-        result = func(*args, **kwargs)
-        logging.debug('recording server send span')
-        trace.record(Annotation.server_send())
-        parent_trace = trace
-        return result
+            #now, log start point
+            logging.debug('recording server recv span')
+            trace.record(Annotation.server_recv())
+            result = func(*args, **kwargs)
+            logging.debug('recording server send span')
+            trace.record(Annotation.server_send())
+            parent_trace = trace
+            return result
+        return wrapper
 
-    return wrapper
+    return zipper
 
 
 
